1df4981234e522693cb45da76888a214ce77b3a8
benjamin333sz <b3szurek@gmail.com>
2026-01-22 10:20:17 +0100

    feat: add requirements python bib

--- detection_value.py
+++ detection_value.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
-
 # nous lisons le fichier csv
 path=r"C:\Users\benja\Desktop\cours_ESME\cours\INGE\INGE_3\semaine_mineur\détection_valeur_aberrante\Input_projet_LVMH.csv"
 
